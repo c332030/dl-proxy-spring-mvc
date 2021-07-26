949b5ca62ff029d8eb70036718972c2075148088
c332030 <c332030@gmail.com>
2021-07-26 23:42:20 +0800

    feat: print headers

--- src/main/java/com/c332030/dl/proxy/springmvc/server/app/controller/ProxyController.java
+++ src/main/java/com/c332030/dl/proxy/springmvc/server/app/controller/ProxyController.java
@@ -1,33 +1,32 @@
 package com.c332030.dl.proxy.springmvc.server.app.controller;
 
 
-import java.io.InputStream;
+import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
+import java.net.URL;
 import java.text.MessageFormat;
-import java.util.Objects;
 
 import org.apache.commons.io.FilenameUtils;
 import org.apache.commons.io.IOUtils;
 import org.apache.commons.lang3.StringUtils;
+
+import org.springframework.util.CollectionUtils;
 
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 
 import lombok.extern.slf4j.Slf4j;
 
 import com.c332030.controller.BaseController;
-
-import okhttp3.Headers;
-import okhttp3.OkHttpClient;
-import okhttp3.Request;
-import okhttp3.Response;
+import com.c332030.util.data.JSONUtils;
+import com.c332030.web.servlet.util.CServletUtils;
 
 /**
  * <p>
@@ -49,9 +48,6 @@ public class ProxyController extends BaseController {
 
     private static final String CONTENT_DISPOSITION_TEMPLATE = "attachment; filename=\"{0}\"";
 
-    @Autowired
-    private OkHttpClient okHttpClient;
-
     @RequestMapping("proxy")
     public ResponseEntity<?> proxy(@RequestParam(name = URL_STR, required = false) String urlStr) {
         try {
@@ -61,46 +57,35 @@ public class ProxyController extends BaseController {
                 return UNKNOWN_PATH;
             }
 
-            Headers.Builder okHeadersBuilder = new Headers.Builder();
-            request.getHeaderNames().asIterator().forEachRemaining(headerName
-                -> okHeadersBuilder.set(headerName, request.getHeader(headerName)));
+            var conn = (HttpURLConnection) new URL(urlStr).openConnection();
+            conn.setInstanceFollowRedirects(true);
+            conn.setConnectTimeout(3000);
+            conn.setRequestMethod(RequestMethod.GET.name());
+            CServletUtils.setHeaders(request, conn);
 
-            Request okRequest = new Request.Builder()
-                .url(urlStr)
-                .headers(okHeadersBuilder.build())
-                .get()
-                .build();
+            log.info("requestProperties: {}, headerFields: {}",
+                JSONUtils.toJson(conn.getRequestProperties()),
+                JSONUtils.toJson(conn.getHeaderFields()));
 
-            // System.out.println("Request: " + JSONUtils.toJson(okRequest.headers()));
-
-            Response okResponse = okHttpClient.newCall(okRequest).execute();
-
-            // System.out.println("Response: " + JSONUtils.toJson(okResponse.headers()));
-
-            var responseCode = okResponse.code();
+            var responseCode = conn.getResponseCode();
             if (200 != responseCode) {
-                return new ResponseEntity<>(okResponse.message(), HttpStatus.valueOf(responseCode));
+                return new ResponseEntity<>(conn.getResponseMessage(), HttpStatus.valueOf(responseCode));
             }
 
-            okResponse.headers().iterator().forEachRemaining(pair -> {
-                response.setHeader(pair.getFirst(), pair.getSecond());
-            });
+            CServletUtils.setHeaders(conn, response);
 
-            var contentDisposition = okResponse.headers().get(HttpHeaders.CONTENT_DISPOSITION);
-            var newContentDisposition = MessageFormat
-                .format(CONTENT_DISPOSITION_TEMPLATE, FilenameUtils.getName(urlStr));
-            log.info("HttpHeaders.CONTENT_DISPOSITION: {}, newContentDisposition: {}", contentDisposition, newContentDisposition);
+            var contentDispositionList = conn.getHeaderFields().get(HttpHeaders.CONTENT_DISPOSITION);
+            var newContentDisposition = MessageFormat.format(CONTENT_DISPOSITION_TEMPLATE,
+                FilenameUtils.getName(urlStr));
+            log.info("HttpHeaders.CONTENT_DISPOSITION: {}, newContentDisposition: {}", contentDispositionList, newContentDisposition);
 
-            if(StringUtils.isEmpty(contentDisposition)
-                || !ATTACHMENT.equals(contentDisposition)
+            if(CollectionUtils.isEmpty(contentDispositionList)
+                || !ATTACHMENT.equals(contentDispositionList.get(0))
             ) {
                 response.setHeader(HttpHeaders.CONTENT_DISPOSITION, newContentDisposition);
             }
 
-            InputStream inputStream = Objects.requireNonNull(okResponse.body()).byteStream();
-            try(inputStream) {
-                IOUtils.copy(inputStream, response.getOutputStream());
-            }
+            IOUtils.copy(conn.getInputStream(), response.getOutputStream());
 
             return null;
         } catch (MalformedURLException e) {
