8cb1ca6e2200df5813944224df6ab9d36ba67858
c332030 <c332030@gmail.com>
2021-11-16 10:06:32 +0800

    feat: remove lombok

--- src/test/kotlin/com/c332030/dl/proxy/test/ProxyTest.kt
+++ src/test/kotlin/com/c332030/dl/proxy/test/ProxyTest.kt
@@ -1,8 +1,6 @@
 package com.c332030.dl.proxy.test
 
 import com.c332030.CLogger
-import lombok.SneakyThrows
-import lombok.extern.slf4j.Slf4j
 import org.apache.commons.io.FilenameUtils
 import org.junit.jupiter.api.Test
 
@@ -13,19 +11,16 @@ import org.junit.jupiter.api.Test
  * @version 1.0
  */
 
-@Slf4j
 class ProxyTest {
 
   private val log = CLogger(this.javaClass)
 
   @Test
-  @SneakyThrows
   fun test() {
     log.info { "result" }
   }
 
   @Test
-  @SneakyThrows
   fun attachment() {
     log.info { "attachment; filename=\"${FilenameUtils.getName("c:\\1.txt")}\"" }
   }
